21b6438566f501b31c43a902330a3fb4fdd1bf73
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-09-28 16:14:54 +0530

    Add signup component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,10 @@
+import SignupComponent from "@/components/SignupComponent";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <SignupComponent />
+    </div>
+  );
 }
---
+++ frontend/components/SignupComponent.tsx
@@ -0,0 +1,9 @@
+import SignupFormDemo from "./signup-form-demo";
+
+export default function SignupComponent() {
+  return (
+    <div className="h-screen flex items-center">
+      <SignupFormDemo />
+    </div>
+  );
+}
