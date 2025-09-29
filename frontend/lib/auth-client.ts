b09df86c2b4a2ebefb313a7c7ceee550131f5498
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-09-29 08:29:18 +0530

    Bug fix

--- frontend/lib/auth-client.ts
+++ frontend/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string, // The base URL of your auth server
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // The base URL of your auth server
     fetchOptions: {
         credentials: "include"
     }
