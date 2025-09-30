3c1afd9a0165698e7322a0cb8eb95ad9963e8e1f
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-09-30 07:16:10 +0530

    add parition

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -27,6 +27,7 @@ export const auth = betterAuth({
             sameSite: 'none',
             secure: true,
             httpOnly: true,
+            partitioned: true,
         }
     },
     plugins: [
